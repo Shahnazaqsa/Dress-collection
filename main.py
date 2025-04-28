486f3b375fa139e4eae2a48128f08bb6ad3847f9
Shahnazaqsa <165772659+Shahnazaqsa@users.noreply.github.com>
2025-04-28 13:12:27 +0500

    Update main.py
    added port number

--- main.py
+++ main.py
@@ -113,4 +113,8 @@ def faq():
     return render_template('faq.html', faqs=faqs)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=True
+    )
